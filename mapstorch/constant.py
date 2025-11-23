Correct Compton normalization and escape energies

mapstorch/constant.py
@@ -1013,13 +1013,10 @@ class Henke:
         denom = energies * 4.0 * np.pi * beta
 
         zeroes = np.where(denom == 0.0)
-        nonzeroes = np.where(denom != 0.0)
         denom[zeroes] = 1e-8
 
-        inverse_mu = np.array((len(energies)))
-
         inverse_mu = 1.239852 / denom
-        if len(zeroes) > 0:
+        if zeroes[0].size > 0:
             inverse_mu[zeroes] = np.inf
 
         return (
@@ -1746,25 +1743,27 @@ def define_constants(info_elements, pileups=None, return_dict=True):
                 e_pars[ii, 2].energy = info_elements[j].xrf["mb"]
                 e_pars[ii, 3].energy = info_elements[j].xrf["mg"]
 
+                ma1_yield = info_elements[j].xrf_abs_yield["ma1"]
+                ma2_yield = info_elements[j].xrf_abs_yield["ma2"]
+                mb_yield = info_elements[j].xrf_abs_yield["mb"]
+                mg_yield = info_elements[j].xrf_abs_yield["mg"]
+
                 e_pars[ii, 0].ratio = 1.0
-                e_pars[ii, 1].ratio = (
-                    info_elements[j].xrf_abs_yield["ma2"]
-                    / info_elements[j].xrf_abs_yield["ma1"]
-                    if info_elements[j].xrf_abs_yield["ma2"] != 0.0
-                    else 1.0
-                )
-                e_pars[ii, 2].ratio = (
-                    info_elements[j].xrf_abs_yield["mb"]
-                    / info_elements[j].xrf_abs_yield["ma1"]
-                    if info_elements[j].xrf_abs_yield["mb"] != 0.0
-                    else 1.0
-                )
-                e_pars[ii, 3].ratio = (
-                    info_elements[j].xrf_abs_yield["mg"]
-                    / info_elements[j].xrf_abs_yield["ma1"]
-                    if info_elements[j].xrf_abs_yield["mg"] != 0.0
-                    else 1.0
-                )
+
+                if ma1_yield != 0.0 and ma2_yield != 0.0:
+                    e_pars[ii, 1].ratio = ma2_yield / ma1_yield
+                else:
+                    e_pars[ii, 1].ratio = 0.0
+
+                if ma1_yield != 0.0 and mb_yield != 0.0:
+                    e_pars[ii, 2].ratio = mb_yield / ma1_yield
+                else:
+                    e_pars[ii, 2].ratio = 0.0
+
+                if ma1_yield != 0.0 and mg_yield != 0.0:
+                    e_pars[ii, 3].ratio = mg_yield / ma1_yield
+                else:
+                    e_pars[ii, 3].ratio = 0.0
 
                 e_pars[ii, 0].type = 7
                 e_pars[ii, 1].type = 7

mapstorch/map.py
@@ -90,38 +90,49 @@ def compton_peak(params, ev, gain, use_step=True, use_tail=False):
         + compton_E * 2.96 * params["FWHM_FANOPRIME"]
     )
     delta_energy = ev - compton_E
-    faktor = 1.0 / (
-        1.0
-        + params["COMPTON_F_STEP"]
-        + params["COMPTON_F_TAIL"]
-        + params["COMPTON_HI_F_TAIL"]
-    )
-    faktor *= 10 ** params["COMPTON_AMPLITUDE"]
+
+    norm = 1.0
+    if use_step and params["COMPTON_F_STEP"] > 0:
+        norm += params["COMPTON_F_STEP"]
+    if use_tail:
+        if params["COMPTON_F_TAIL"] > 0:
+            norm += params["COMPTON_F_TAIL"]
+        if params["COMPTON_HI_F_TAIL"] > 0:
+            norm += params["COMPTON_HI_F_TAIL"]
+
+    faktor = 10 ** params["COMPTON_AMPLITUDE"] / norm
+
     counts = faktor * peak(gain, sigma * params["COMPTON_FWHM_CORR"], delta_energy)
-    if params["COMPTON_F_STEP"] > 0 and use_step:
+
+    if use_step and params["COMPTON_F_STEP"] > 0:
         counts.add_(
-            (faktor * params["COMPTON_F_STEP"])
-            * step(gain, sigma, delta_energy, compton_E)
+            faktor * params["COMPTON_F_STEP"] * step(gain, sigma, delta_energy, compton_E)
         )
     if use_tail:
-        counts.add_(
-            (faktor * params["COMPTON_F_TAIL"])
-            * tail(gain, sigma, delta_energy, params["COMPTON_GAMMA"])
-        )
-        counts.add_(
-            (faktor * params["COMPTON_HI_F_TAIL"])
-            * tail(gain, sigma, -delta_energy, params["COMPTON_HI_GAMMA"])
-        )
+        if params["COMPTON_F_TAIL"] > 0:
+            counts.add_(
+                faktor
+                * params["COMPTON_F_TAIL"]
+                * tail(gain, sigma, delta_energy, params["COMPTON_GAMMA"])
+            )
+        if params["COMPTON_HI_F_TAIL"] > 0:
+            counts.add_(
+                faktor
+                * params["COMPTON_HI_F_TAIL"]
+                * tail(gain, sigma, -delta_energy, params["COMPTON_HI_GAMMA"])
+            )
     return counts
 
 
 def escape_peak(spectra, ev, escape_factor, device, detector_type="Si"):
     if detector_type == "Si":
-        detector_K_edge = 1.73998  # keV
+        # Approximate Si Kα1 energy (keV)
+        detector_escape_E = 1.73998
     elif detector_type == "Ge":
-        detector_K_edge = 11.103  # keV
+        # Approximate Ge Kα1 energy (keV)
+        detector_escape_E = 9.886
     else:
-        detector_K_edge = 1.73998
+        detector_escape_E = 1.73998
 
     escape_spec = torch.zeros_like(spectra, device=device)
 
@@ -132,7 +143,7 @@ def escape_peak(spectra, ev, escape_factor, device, detector_type="Si"):
     if delta_e <= 0:
         return escape_spec
 
-    bins = int(round(detector_K_edge / delta_e))
+    bins = int(round(detector_escape_E / delta_e))
     if bins < len(ev):
         escape_spec[: len(ev) - bins] = spectra[bins:] * escape_factor
 